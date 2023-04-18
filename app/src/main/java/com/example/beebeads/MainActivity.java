f7b632639bf38e66935f9752e45a7792dc2abd02
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-04-18 08:11:19 -0700

    testing creating new users in db Working!

--- app/src/main/java/com/example/beebeads/MainActivity.java
+++ app/src/main/java/com/example/beebeads/MainActivity.java
@@ -6,38 +6,52 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private UserViewModel userViewModel;
-    private TextView textView;
+
+    private EditText enterUsername;
+    private EditText enterPassword;
+
+    private Button loginButton;
+    private Button signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.test);
-
         userViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication()).
                 create(UserViewModel.class);
 
-        userViewModel.getAllUsers().observe(this, users -> {
-            StringBuilder builder = new StringBuilder();
-            for (User user : users){
-                Log.d("User", "Hey " + user.getUsername() + " is this your password? " +
-                        user.getPassword() + " admin? " + user.getAdmin() + " userId? " + user.getUserId());
-                builder.append(user.getUsername()).append(" ");
+        enterUsername = findViewById(R.id.username);
+        enterPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        signUpButton = findViewById(R.id.signup);
+
+        signUpButton.setOnClickListener(view -> {
+
+            if (!TextUtils.isEmpty(enterUsername.getText())
+                    && !TextUtils.isEmpty(enterPassword.getText())) {
+                User user = new User(enterUsername.getText().toString(), enterPassword.getText().
+                        toString(), 0);
+
+                UserViewModel.insert(user);
+            } else {
+                Toast.makeText(this, R.string.empty, Toast.LENGTH_SHORT).show();
             }
-            textView.setText(builder.toString());
-
-
         });
-
 
     }
 }
